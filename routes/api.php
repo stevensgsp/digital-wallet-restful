b0f342b781014d22e706e13dca908c3b05dc11c4
Steven Sucre <steven.g.s.p@gmail.com>
2022-09-17 09:32:21 -0400

    Installation of codedredd/laravel-soap.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/clients/register', [ClientController::class, 'register']);
+Route::post('/wallets/recharge', [WalletController::class, 'rechargeWallet']);
+Route::post('/wallets/check-balance', [WalletController::class, 'checkBalance']);
+Route::post('/wallets/checkout', [WalletController::class, 'checkout']);
+Route::post('/wallets/confirm-checkout', [WalletController::class, 'confirmCheckout']);
